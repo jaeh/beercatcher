f02d8ab9846b369186504df73c412a189e1660f4
Jascha <jascha@jaeh.at>
2019-05-14 03:21:36 +0200

    get running again. not much to see

--- app.js
+++ app.js
@@ -15,19 +15,9 @@ var express = require('express')
 app.set('port', process.env.PORT || 2323);
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.set('view engine', 'pug');
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 
